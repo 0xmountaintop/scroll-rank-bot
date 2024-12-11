57ee9caee765c8683a11fe9be9c8b1accf81da10
0xmountaintop <haoyu@protonmail.com>
2024-12-11 22:41:58 +1100

    add shillTexts

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"sort"
 	"strings"
 	"sync"
@@ -29,6 +30,16 @@ type Bot struct {
 	// gasCacheDur time.Duration
 	// lastGasTime time.Time
 	// cachedGas   string
+}
+
+var shilTexts = []string{
+	"不需要那么多，1000个SCR就够了。你会发现，人这一辈子，根本不需要那么多钱。",
+	"SCR本身没有风险。SCR最大的风险在于，你以为有风险，可能拿不住。",
+	"SCR涨不需要分析原因。太阳的升起，并不是因为鸡鸣。",
+	"几年后再看SCR的k线，整个2024年就是一条直线而已。",
+	"上线交易所不是SCR的利好。SCR上线交易所是交易所的利好。",
+	"我并不觉得SOL会成为SCR的对手，超越SOL这样的目标，对SCR来说显得太小。",
+	"未来，你会发现你根本不在乎生活品质，持SCR在手就是最高生活品质。",
 }
 
 func New(token string) (*Bot, error) {
@@ -82,6 +93,11 @@ func (b *Bot) handleUpdates(updates tgbotapi.UpdatesChannel) {
 		case "get_current_gas_price":
 			gasPrices := b.gasService.FetchAllPrices()
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, b.formatGasPrices(gasPrices))
+			b.api.Send(msg)
+
+		case "shill_scroll":
+			shillText := shilTexts[rand.Intn(len(shilTexts))]
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, shillText)
 			b.api.Send(msg)
 		}
 	}
