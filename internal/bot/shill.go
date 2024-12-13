9be29722b39eabf06ba5b778964793e38e0496e7
0xmountaintop <haoyu@protonmail.com>
2024-12-13 22:19:31 +1100

    reorg folder structure

---
+++ internal/bot/shill.go
@@ -0,0 +1,26 @@
+package bot
+
+var shilTexts = []string{
+	`不需要那么多，1000个SCR就够了。
+你会发现，人这一辈子，根本不需要那么多钱。`,
+
+	`SCR本身没有风险。
+SCR最大的风险在于，你以为有风险，可能拿不住。`,
+
+	`SCR涨不需要分析原因。
+太阳的升起，并不是因为鸡鸣。`,
+
+	`几年后再看SCR的k线，整个2024年就是一条直线而已。`,
+
+	`上线交易所不是SCR的利好。
+SCR上线交易所是交易所的利好。`,
+
+	`我并不觉得SOL会成为SCR的对手，
+超越SOL这样的目标，对SCR来说显得太小。`,
+
+	`未来，你会发现你根本不在乎生活品质，持SCR在手就是最高生活品质。`,
+
+	`I'll buy as much $SCR as you have,
+right now, at 0.7.
+Sell me all you want. Then go fuck off.`,
+}
